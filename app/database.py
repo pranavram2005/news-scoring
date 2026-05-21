b036ff5abd09e4dbad29e9ef62ea8a4794cc416e
Deepti <chennaisunday@gmail.com>
2026-05-21 12:35:31 +0530

    Initial clean commit 1

--- app/database.py
+++ app/database.py
@@ -12,6 +12,9 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not configured.")
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
+
 
 class Base(DeclarativeBase):
     pass
